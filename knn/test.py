4aa66ee5980a682e05baf3725daf2ad074607f49
github.token <418406100@qq.com>
2017-09-18 22:14:47 +0800

    knn算好的约会人选

--- knn/test.py
+++ knn/test.py
@@ -3,12 +3,29 @@ from numpy import *
 import operator
 import  matplotlib.pyplot as plt
 
-X=np.linspace(-np.pi,np.pi,256,endpoint=True)
-(C,S) = np.cos(X),np.sin(X)
-fig = plt.figure(figsize=(10,6),dpi=80)
-plt.plot(X,C,'b-',lw=2.5)
-plt.plot(X,C,'r-',lw=2.5)
-print()
+# X=np.linspace(-np.pi,np.pi,256,endpoint=True)
+# (C,S) = np.cos(X),np.sin(X)
+# fig = plt.figure(figsize=(10,6),dpi=80)
+# plt.plot(X,C,'b-',lw=2.5)
+# plt.plot(X,C,'r-',lw=2.5)
+# print()
+# dataSet = arange(15).reshape(5,3)
+# print(dataSet)
+# minVals = dataSet.min(0)
+# maxVals = dataSet.max(0)
+# ranges = maxVals-minVals
+# normDataSet = zeros(shape(dataSet))
+# m=dataSet.shape[0]
+# normDataSet =dataSet-tile(minVals,(m,1))
+# normDataSet =normDataSet/tile(ranges,(m,1))
+# print(normDataSet)
+
+# dd=np.mat([4,5,1])
+# dd1 = dd.argsort()
+# dd2 = dd1.argsort()
+# print(dd)
+# print(dd1)
+# print(dd2)
 
 
 
